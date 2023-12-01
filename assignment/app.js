86734554d970d0865401414c2b9d84d29eb6d445
hammad232311 <asad.moon021@gmail.com>
2023-12-01 07:44:16 -0800

    work complete

---
+++ assignment/app.js
@@ -0,0 +1,41 @@
+ // //alert('hellpjavascript');
+// //console.log("hello javascript");
+// var personName = ("50");
+// alert(personName);
+// var personName = ("85");
+// alert(personName);
+
+// var personName = ("asad");
+// console.log(personName);
+// var personName = ("karachi");
+// console.log(personName);
+
+// var A = 10;
+// var B = 20;
+// var C = 3
+// ;
+// console.log(A-B*C);
+
+// // var A = 10;
+// var B = A++;
+// console.log(A, "A value");
+// console.log(B, "B value");
+
+var userName = prompt("Enter Your Name");
+var userName = prompt("Enter Your Class Name");
+
+var a = +prompt("Physics");
+var b = +prompt("Mathematics");
+var c = +prompt("English");
+var d = +prompt("Boilogy");
+var e = +prompt("Urdu");
+
+var f = 500;
+var percentage = ( (a + b + c + d + e ) / f) *100;
+
+alert (percentage);
+if (percentage >= 33 ){
+    alert ("Congratulation You are pass!")
+} else{
+    alert ("you are failed!")
+}
